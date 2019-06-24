9d200963fa1cbac8bea1b13326f8a711cf8b2961
filippopiconese <filippopiconese@hotmail.it>
2019-06-24 20:17:52 +0200

    fix(download media): Now the media can have the field type (tale or lullaby). Also fixed some errors

--- server/controllers/media.controller.js
+++ server/controllers/media.controller.js
@@ -17,7 +17,7 @@ module.exports = {
       const gfs = gridfs(connection.db, mongoose.mongo)
 
       gfs.files.find({}).toArray((err, files) => {
-        if (err) {
+        if (err || files.length === 0) {
           console.info(status[404])
           return res.status(404).json({ error: 'Stories not found' })
         }
@@ -37,14 +37,13 @@ module.exports = {
 
     const connection = mongoose.connection
     connection.on('error', console.error.bind(console, 'connection error:'))
-
     connection.once('open', () => {
       const gfs = gridfs(connection.db, mongoose.mongo)
 
       gfs.files.find({ filename: req.query }).toArray((err, files) => {
-        if (err) {
+        if (err || files.length === 0) {
           console.info(status[404])
-          return res.status(404).json({ error: 'Stories not found' })
+          return res.status(404).json({ error: 'Story not found' })
         }
 
         const readstream = gfs.createReadStream({ filename: req.query })
@@ -54,14 +53,25 @@ module.exports = {
   }
 }
 
-// { "name" : "Music.opus", "length" : "1.30", "price" : "0.5" }
-// http://localhost:5000/users/dashboard/media/download?name=Music.opus&length=1.30&price=0.5
+// Tales
 
-// { "name" : "Alice-In-Wonderland.opus", "length" : "14.04", "price" : "3" }
-// http://localhost:5000/users/dashboard/media/download?name=Alice-In-Wonderland.opus&length=14.04&price=3
+// { "name" : "Alice-In-Wonderland.opus", "length" : "14.04", "price" : "3", type: 'tale }
+// http://localhost:5000/users/dashboard/media/download?name=Alice-In-Wonderland.opus&length=14.04&price=3&type=tale
 
-// { "name" : "The-Wonderful-Wizard-Of-Oz.opus", "length" : "9.52", "price" : "2" }
-// http://localhost:5000/users/dashboard/media/download?name=The-Wonderful-Wizard-Of-Oz.opus&length=9.52&price=2
+// { "name" : "Pinocchio.opus", "length" : "5.29", "price" : "1.5", type: 'tale }
+// http://localhost:5000/users/dashboard/media/download?name=Pinocchio.opus&length=5.29&price=1.5&type=tale
 
-// { "name" : "Pinocchio.opus", "length" : "5.29", "price" : "1" }
-// http://localhost:5000/users/dashboard/media/download?name=Pinocchio.opus&length=5.29&price=1
+// { "name" : "The-Wonderful-Wizard-Of-Oz.opus", "length" : "9.52", "price" : "2", type: 'tale }
+// http://localhost:5000/users/dashboard/media/download?name=The-Wonderful-Wizard-Of-Oz.opus&length=9.52&price=2&type=tale
+
+
+// Lullabies
+
+// { "name" : "Calm.opus", "length" : "2.03", "price" : "0.5", type: 'lullaby }
+// http://localhost:5000/users/dashboard/media/download?name=Calm.opus&length=2.03&price=0.5&type=lullaby
+
+// { "name" : "Babies-With-Mozart.opus", "length" : "3.31", "price" : "1", type: 'lullaby }
+// http://localhost:5000/users/dashboard/media/download?name=Babies-With-Mozart.opus&length=3.31&price=1&type=lullaby
+
+// { "name" : "Brahms-Lullaby.opus", "length" : "3.34", "price" : "1", type: 'lullaby }
+// http://localhost:5000/users/dashboard/media/download?name=Brahms-Lullaby.opus&length=3.34&price=1&type=lullaby
